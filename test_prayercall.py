2a7fa8cc02e5e7d79cb1c04b66f121c104bcfe12
Youssef Bitar <youssefbitar@Youssefs-MacBook-Pro-2.local>
2023-12-24 19:52:28 -0600

    fixed the sleep time to be accurate

--- test_prayercall.py
+++ test_prayercall.py
@@ -1,7 +1,7 @@
 import time
 from unittest import TestCase
 from prayercall import PrayerCall
-from datetime import datetime
+from datetime import datetime, timedelta
 
 class TestPrayerCall(TestCase):
     def test_get_next_day_date_1(self):
@@ -18,14 +18,14 @@ class TestPrayerCall(TestCase):
         current_time = time.strptime(datetime_str, '%Y-%m-%d %H:%M:%S')
         athan = PrayerCall(1.0, -1.0, -1, 'ISNA')
         calculated_time = athan.calculate_time_to_sleep(current_time, current_time, datetime.strptime('08:35', '%H:%M'))
-        self.assertEqual(270, calculated_time)
+        self.assertEqual(305, calculated_time)
 
     def test_calculate_time_to_sleep_2(self):
         datetime_str = "2023-12-25 11:58:00"
         current_time = time.strptime(datetime_str, '%Y-%m-%d %H:%M:%S')
         athan = PrayerCall(1.0, -1.0, -1, 'ISNA')
         calculated_time = athan.calculate_time_to_sleep(current_time, current_time, datetime.strptime('11:59', '%H:%M'))
-        self.assertEqual(30, calculated_time)
+        self.assertEqual(65, calculated_time)
 
     def test_calculate_time_to_sleep_3(self):
         datetime_str = "2023-12-25 23:58:45"
@@ -34,5 +34,26 @@ class TestPrayerCall(TestCase):
         time_to_update = time.strptime(datetime_str_to_update, '%Y-%m-%d %H:%M:%S')
         athan = PrayerCall(1.0, -1.0, -1, 'ISNA')
         calculated_time = athan.calculate_time_to_sleep(current_time, time_to_update, datetime.strptime('05:09', '%H:%M'))
-        self.assertEqual(18630, calculated_time)
+        self.assertEqual(18620, calculated_time)
+
+    def test_main_1(self):
+        datetime_str = "2023-12-25 18:25:45"
+        current_time = time.strptime(datetime_str, '%Y-%m-%d %H:%M:%S')
+        datetime_str_to_update = "2023-12-25 18:25:45"
+        time_to_update = time.strptime(datetime_str_to_update, '%Y-%m-%d %H:%M:%S')
+        athan = PrayerCall(1.0, -1.0, -1, 'ISNA')
+        athan.next_prayer_name = 'isha'
+        calculated_time = athan.calculate_time_to_sleep(current_time, time_to_update, datetime.strptime('18:27', '%H:%M'))
+        # Get the current date
+        # Example date and time string
+        date_time_str = '2023-12-25 18:25:45'  # Replace this with your date and time string
+
+        # Define the format of the date and time string
+        date_time_format = '%Y-%m-%d %H:%M:%S'  # Adjust this format to match your string
+
+        # Create a datetime object from the string using strptime()
+        date_time_obj = datetime.strptime(date_time_str, date_time_format)
+
+        date_time_obj + timedelta(seconds=calculated_time)
+        self.assertEqual(80, calculated_time)
 
